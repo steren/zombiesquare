711d87149e84c2b1ff4de58365c291350f1cfec4
duchere <duchere@gmail.com>
2011-09-17 17:56:08 +0200

    four squre login, files modified

--- zombiesquare/app/controllers/Application.java
+++ zombiesquare/app/controllers/Application.java
@@ -2,15 +2,28 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import requests.HTTPRequestPoster;
 import java.util.*;
 
-import models.*;
+import json.model.AccessToken;
+
+import com.google.gson.Gson;
 
 public class Application extends Controller {
 
     public static void index() {
+    	renderArgs.put("fourSquareConnectUrl", Parameters.fourSquareConnectUrl());
         render();
+    }
+    
+    public static void authenticate(String code) {
+    	Gson gson = new Gson();
+    	AccessToken access_token = gson.fromJson(
+    			HTTPRequestPoster.sendGetRequest(Parameters.accessTokenRequestUrl(), Parameters.accessTokenRequestUrlParameters(code))
+    			,AccessToken.class);
+    	renderArgs.put("response", access_token.getToken());
+    	//TODO - authentication request
+    	render();
     }
 
 }

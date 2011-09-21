131a82812633f9b1390bde8a776ca2635d749689
Steren <steren.giannini@gmail.com>
2011-09-21 23:54:07 +0200

    3 max weapons

--- zombiesquare/app/parameters/GameParameters.java
+++ zombiesquare/app/parameters/GameParameters.java
@@ -8,6 +8,6 @@ public class GameParameters {
 	
 	public static final int SCORE_CONTAMINATE_VENUE = 1;
 	public static final int SCORE_DECONTAMINATE_VENUE = 10;
-	public static final int SCORE_DECONTAMINATE_ZOMBIE = 5;
+	public static final int SCORE_DECONTAMINATE_ZOMBIE = 3;
 	
 }

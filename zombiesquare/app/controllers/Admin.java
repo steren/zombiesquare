b32333ecfc57d9ec919a1d7e376694faed9187bb
Steren <steren.giannini@gmail.com>
2011-09-23 11:55:19 +0200

    weapon number max : 3

--- zombiesquare/app/controllers/Admin.java
+++ zombiesquare/app/controllers/Admin.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import parameters.GameParameters;
 import play.*;
 import play.data.validation.Email;
 import play.data.validation.Required;
@@ -17,5 +18,20 @@ public class Admin extends Controller {
     public static void players() {
         Collection<Player> players = Player.findAll();
         render(players);
+    }
+    
+    /**
+     * Make sure nobody has more weapon than the authorized number
+     */
+    public static void checkWeaponNumber() {
+        Collection<Player> players = Player.findAll();
+        
+        for(Player player : players) {
+        	if(player.weapons > GameParameters.maxCountWeapons) {
+        		player.weapons = new Long(GameParameters.maxCountWeapons);
+        		player.save();
+        	}
+        }
+        players();
     }
 }
--- zombiesquare/app/parameters/GameParameters.java
+++ zombiesquare/app/parameters/GameParameters.java
@@ -3,7 +3,8 @@ package parameters;
 public class GameParameters {
 	/** probability between 0 and 1 to win a weapon */
 	public static double getWeaponProbability = 0.4;
-	public static int maxCountWeapons = 5;
+	/** maximum number of weapons a survivor can carry, he can have [0 ... maxCountWeapons] weapons. */
+	public static int maxCountWeapons = 3;
 	public static int costKeepLive = 1;
 	public static int costSaveAll = 2;
 	
